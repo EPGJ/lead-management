60ca9abff287565b5da642d48467bc5124473967
Elias Junior <eliasepgj@gmail.com>
2022-04-12 09:54:50 -0300

    Reorganizando função de submit na página nova lead e voltando ao layout antigo

--- src/pages/addLead/AddLead.jsx
+++ src/pages/addLead/AddLead.jsx
@@ -16,149 +16,120 @@ import phoneMask from '../../utils/phoneMask';
 function AddLead() {
 
   const navigate = useNavigate();
-
   const defaultValues = initialState.leadForm;
 
 
-  const handleSubmit = (values, { setSubmitting }) => {
-    setTimeout(() => {
-      submit(values, { setSubmitting });
-      alert('sucesso ao cadastrar lead!!');
-      navigate('/leads');
-    }, 500);
-  }
-
   return (
-    <Container component="main" maxWidth="xs">
-
+    <Container maxWidth="lg">
       <Box
         sx={{
           marginTop: 8,
-          marginBottom: 2,
           display: 'flex',
           flexDirection: 'column',
           alignItems: 'center',
         }}
       >
-
-
-        <Typography component="h1" variant="h5">Novo Lead</Typography>
-
-        <Formik
-          initialValues={defaultValues}
-          validate={dataValidation}
-          onSubmit={(values, { setSubmitting }) =>
-            handleSubmit(values, { setSubmitting })
-          }
-        >
-          {({
-            submitForm,
-            isSubmitting,
-            errors,
-            values,
-            touched,
-            status,
-            setFieldTouched,
-            setFieldValue,
-          }) => (
-            <Form style={{ width: '100%' }}>
-              <Grid
-                container
-                alignItems="center"
-                justifyContent="center"
-                spacing={2}
-                item
-              >
-
-                <Grid container spacing={3}  sm={8} >
-
-                  <Grid item xs={12} >
-                    <label htmlFor='name'>Nome *</label>
-                    <Field
-                      component={TextField}
-                      name="name"
-                      type="text"
-                      id="name"
-                      fullWidth
-                      required
-                    />
-                  </Grid>
-                  <Grid item xs={12}>
-                    <label htmlFor='phone'>Telefone *</label>
-                    <Field
-                      component={TextField}
-                      name="phone"
-                      type="text"
-                      id="phone"
-                      onChange={(event) => setFieldValue('phone', phoneMask(event.target.value))}
-                      fullWidth
-                      required
-                    />
-                  </Grid>
-                  <Grid item xs={12}>
-                    <label htmlFor='email'>Email *</label>
-                    <Field
-                      component={TextField}
-                      name="email"
-                      type="email"
-                      id="email"
-                      fullWidth
-                      required
-                    />
-                  </Grid>
-
-                </Grid>
-
-                <Grid item xs={12} sm={4}>
-
-                  <Opportunities
-                    errors={errors}
-                    values={values}
-                    touched={touched}
-                    setFieldTouched={setFieldTouched}
-                    status={status}
-                  />
-
-                </Grid>
-
-                <Grid
-                  container
-                  alignItems="center"
-                  justifyContent="center"
-                  item
-                  xs={12}
-                >
-
-                  <Grid item xs={8}>
-                    <Button variant="outlined" onClick={() => navigate('/leads')}>
-                      <ArrowBackIcon />
-                      Voltar
-                    </Button>
-                  </Grid>
-
-                  <Grid item xs={4}>
-
-                    <Button
-                      type="submit"
-                      variant="contained"
-                      color="primary"
-                      disabled={isSubmitting}
-                      onClick={submitForm}
-                      fullWidth
+        <Grid container spacing={2}>
+          <Grid item xs={12}>
+            <Typography component="h1" variant="h5">Novo Lead</Typography>
+          </Grid>
+          <Grid item xs={12}>
+            <Button variant="outlined" onClick={() => navigate('/leads')}>
+              <ArrowBackIcon />
+              Voltar
+            </Button>
+          </Grid>
+          <Grid container item xs={12}>
+            <Formik
+              initialValues={defaultValues}
+              validate={dataValidation}
+              onSubmit={(values, { setSubmitting }) =>
+                submit(values, { setSubmitting }, navigate)
+              }
+            >
+              {({
+                submitForm,
+                isSubmitting,
+                errors,
+                values,
+                touched,
+                status,
+                setFieldTouched,
+                setFieldValue,
+              }) => (
+                <Form style={{ width: '100%' }}>
+                  <Grid container spacing={2} justifyContent="space-between">
+                    <Grid container spacing={3} item xs={12} sm={8} md={5}>
+                      <Grid item xs={12} >
+                        <label htmlFor='name'>Nome *</label>
+                        <Field
+                          component={TextField}
+                          name="name"
+                          type="text"
+                          id="name"
+                          fullWidth
+                          required
+                        />
+                      </Grid>
+                      <Grid item xs={12}>
+                        <label htmlFor='phone'>Telefone *</label>
+                        <Field
+                          component={TextField}
+                          name="phone"
+                          type="text"
+                          id="phone"
+                          onChange={(event) => setFieldValue('phone', phoneMask(event.target.value))}
+                          fullWidth
+                          required
+                        />
+                      </Grid>
+                      <Grid item xs={12}>
+                        <label htmlFor='email'>Email *</label>
+                        <Field
+                          component={TextField}
+                          name="email"
+                          type="email"
+                          id="email"
+                          fullWidth
+                          required
+                        />
+                      </Grid>
+                    </Grid>
+                    <Grid item xs={12} sm={4}>
+                      <Opportunities
+                        errors={errors}
+                        values={values}
+                        touched={touched}
+                        setFieldTouched={setFieldTouched}
+                        status={status}
+                      />
+                    </Grid>
+                    <Grid
+                      container
+                      alignItems="right"
+                      justifyContent="right"
+                      item
+                      xs={12}
                     >
-                      SALVAR
-                    </Button>
-
+                      <Grid item xs={4}>
+                        <Button
+                          type="submit"
+                          variant="contained"
+                          color="primary"
+                          disabled={isSubmitting}
+                          onClick={submitForm}
+                          fullWidth
+                        >
+                          SALVAR
+                        </Button>
+                      </Grid>
+                    </Grid>
                   </Grid>
-
-
-                </Grid>
-              </Grid>
-            </Form>
-          )}
-        </Formik>
-
-
+                </Form>
+              )}
+            </Formik>
+          </Grid>
+        </Grid>
       </Box>
     </Container>
   );
--- src/services/leadService.js
+++ src/services/leadService.js
@@ -6,21 +6,29 @@ const parseOpportunities = (values) => {
     return opportunities;
 };
 
-export const submit = (values, { setSubmitting }) => {
+export const submit = (values, { setSubmitting },navigate) => {
+    
     setTimeout(() => {
         setSubmitting(false);
         let leads = JSON.parse(window.localStorage.getItem('leads') ?? '[]');
-        leads = [
-            ...leads,
-            {
-                id: leads.length + 1,
-                name: values.name,
-                phone: values.phone,
-                email: values.email,
-                status: 'Cliente em Potencial',
-                opportunities: parseOpportunities(values),
-            },
-        ];
-        window.localStorage.setItem('leads', JSON.stringify(leads));
+        try {
+            leads = [
+                ...leads,
+                {
+                    id: leads.length + 1,
+                    name: values.name,
+                    phone: values.phone,
+                    email: values.email,
+                    status: 'Cliente em Potencial',
+                    opportunities: parseOpportunities(values),
+                },
+            ];
+            window.localStorage.setItem('leads', JSON.stringify(leads));
+            navigate('/leads');
+            alert('Sucesso!');
+        } catch (error) {
+            alert('Erro ao cadastrar: ' + error);
+        }
     }, 500);
-};
+      
+  };
